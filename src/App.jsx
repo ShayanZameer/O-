bb85d0ff0c57ef5bf20127cc06ebac1ad3d31208
Shayan Zameer <alizadishah2@gmail.com>
2024-06-21 18:01:26 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <div>
+
+      <div>
+
+        <Home/>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
